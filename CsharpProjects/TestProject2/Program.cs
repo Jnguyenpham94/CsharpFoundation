245035931841ef8aa29d89b9e78b07adc95575da
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-08-31 18:19:27 -0700

    part 2 DONE

---
+++ CsharpProjects/TestProject2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Dice rolls:");
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+Random dice2 = new Random();
+int result = dice2.Next();
+Console.WriteLine(result);
+
+Random dice3 = new Random();
+int roll1 = dice3.Next();
+int roll2 = dice3.Next(101);
+int roll3 = dice3.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine($"The larger value is: {largerValue}");

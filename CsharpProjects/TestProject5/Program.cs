89d21b1758ba82f9cafc1bea569a18be06d311a1
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-01 16:46:22 -0700

    part 5 DONE

---
+++ CsharpProjects/TestProject5/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+
+// Reverse the chars
+Array.Reverse(charMessage);
+
+int count = 0;
+
+// count the o's
+foreach (char i in charMessage)
+{
+    if (i == 'o')
+    {
+        count++;
+    }
+}
+
+// convert it back to a string
+string new_message = new String(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {count} times.");

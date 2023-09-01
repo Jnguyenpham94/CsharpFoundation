a658b27a7cdf4f01067e3649f592d23b82af433c
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-08-31 19:20:43 -0700

    part 4 DONE

---
+++ CsharpProjects/TestProject4/challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+B123
+C234
+A345
+C15
+B177
+G3003
+C235
+B179
+*/
+
+string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string item in fraudulentOrderIDs)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine(item);
+    }
+}

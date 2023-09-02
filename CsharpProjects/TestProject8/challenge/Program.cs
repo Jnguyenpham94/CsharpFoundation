7b3a45cdfadc01ae37aee5364301ca4de0d2cbdc
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-01 18:57:55 -0700

    part 8 DONE

--- CsharpProjects/TestProject8/challenge/Program.cs
+++ CsharpProjects/TestProject8/challenge/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+//challenge 1 coin flip
+Random coin = new Random();
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+
+//challenge 2 decision logic
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}

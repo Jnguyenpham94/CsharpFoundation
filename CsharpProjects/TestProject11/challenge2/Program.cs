e0235643f12c915179ebb160637f310266dc45f8
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-03 23:45:00 -0700

    part 11 DONE

---
+++ CsharpProjects/TestProject11/challenge2/Program.cs
@@ -0,0 +1,21 @@
+﻿string? readResult;
+string? end;
+
+do
+{
+    Console.WriteLine("Enter a string:");
+    readResult = Console.ReadLine();
+    System.Console.WriteLine($"Read result is: {readResult} | end y/n?");
+    end = Console.ReadLine();
+    if (end == "y") break;
+    else continue;
+} while (readResult != null);
+
+// capture user input in a string variable named readResult
+
+int numericValue = 0;
+bool validNumber = false;
+
+validNumber = int.TryParse(readResult, out numericValue);
+System.Console.WriteLine(validNumber);
+System.Console.WriteLine(numericValue);
---
+++ CsharpProjects/TestProject11/challenge2/part1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+string? input;
+int numValue = 0;
+bool validNumber = false;
+System.Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    input = Console.ReadLine();
+    if (input != null)
+    {
+
+    }
+
+    validNumber = int.TryParse(input, out numValue);
+
+    if (validNumber == true)
+    {
+        if (numValue <= 5 || numValue >= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value {numValue} has been accepted.");

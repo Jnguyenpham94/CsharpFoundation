207db886bd914884894cbd6ebbf21c6bba454d5c
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-05 18:31:06 -0700

    part 14 DONE

---
+++ CsharpProjects/TestProject14/Program.cs
@@ -0,0 +1,76 @@
+﻿
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+// string value = "102";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+
+// Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+//convert values and output individual results
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+// string message = "";
+// decimal total = 0m;
+// foreach (string item in values)
+// {
+//     decimal number = 0;
+//     if (decimal.TryParse(item, out number))
+//         total += number;
+//     else
+//     {
+//         message += item.ToString();
+//     }
+
+// }
+// System.Console.WriteLine($"Message: {message}");
+// System.Console.WriteLine($"Total: {total}");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+int result1 = Convert.ToInt32((decimal)value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");

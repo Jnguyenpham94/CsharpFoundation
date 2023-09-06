fef0d02333ac3fead56f824e1b3fe2f9385d1370
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-05 20:52:39 -0700

    part 15 DONE
    challenge 1 & 2 DONE

---
+++ CsharpProjects/TestProject15/challenge2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//parse string into string array and change errors
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderId = orderStream.Split(',');
+Array.Sort(orderId);
+for (int i = 0; i < orderId.Length; i++)
+{
+    if (orderId[i].Length != 4)
+    {
+        orderId[i] = orderId[i] + "\t- Error";
+    }
+}
+
+foreach (string item in orderId)
+{
+    System.Console.WriteLine(item);
+}

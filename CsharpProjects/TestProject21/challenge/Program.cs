1d987ad93aaeddf4d2922b9f851367b332932aad
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-09 20:45:32 -0700

    part 21 DONE

---
+++ CsharpProjects/TestProject21/challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+string internalDomain = "contoso.com";
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    // display internal email addresses
+    DisplayEmail(corporate[i, 0], corporate[i, 1], internalDomain);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    // display external email addresses
+    DisplayEmail(external[i, 0], external[i, 1], externalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+    string email = first.Substring(0, 2).ToLower() + last.ToLower() + domain;
+    Console.WriteLine($"{email}");
+}

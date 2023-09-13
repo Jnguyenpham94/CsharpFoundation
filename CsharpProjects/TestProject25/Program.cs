4384015367cb2c8ee633f9e743d3961dde3af790
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-12 18:12:12 -0700

    part 25
    exercises START

---
+++ CsharpProjects/TestProject25/Program.cs
@@ -0,0 +1,162 @@
+﻿// try
+// {
+//     // Step 1: code execution begins
+//     try
+//     {
+//         // Step 2: an exception occurs here
+//     }
+//     finally
+//     {
+//         // Step 4: the system executes the finally code block associated with the try statement where the exception occurred
+//     }
+
+// }
+// catch // Step 3: the system finds a catch clause that can handle the exception
+// {
+//     // Step 5: the system transfers control to the first line of the catch code block
+// }
+
+// string[] names = { "Dog", "Cat", "Fish" };
+// Object[] objs = (Object[])names;
+
+// Object obj = (Object)13;
+// objs[2] = obj; // ArrayTypeMismatchException occurs
+
+// int number1 = 3000;
+// int number2 = 0;
+// Console.WriteLine(number1 / number2); // DivideByZeroException occurs
+
+// double float1 = 3000.0;
+// double float2 = 0.0;
+// int number1 = 3000;
+// int number2 = 0;
+
+// try
+// {
+//     Console.WriteLine(float1 / float2);
+//     Console.WriteLine(number1 / number2);
+// }
+// catch (System.Exception)
+// {
+
+//     Console.WriteLine("An exception has been caught");
+// }
+
+// Console.WriteLine("Exit program");
+
+// try
+// {
+//     Process1();
+// }
+// catch
+// {
+//     Console.WriteLine("An exception has occurred");
+// }
+
+// Console.WriteLine("Exit program");
+
+// static void Process1()
+// {
+//     try
+//     {
+//         WriteMessage();
+//     }
+//     catch (System.Exception)
+//     {
+
+//         System.Console.WriteLine("Exception caught in Process1");
+//     }
+
+// }
+
+// static void WriteMessage()
+// {
+//     double float1 = 3000.0;
+//     double float2 = 0.0;
+//     int number1 = 3000;
+//     int number2 = 0;
+
+//     Console.WriteLine(float1 / float2);
+//     Console.WriteLine(number1 / number2);
+// }
+
+try
+{
+    OperatingProcedure1();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Exiting application.");
+}
+
+static void OperatingProcedure1()
+{
+    string[][] userEnteredValues = new string[][]
+    {
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+    };
+
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            BusinessProcess1(userEntries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.StackTrace.Contains("BusinessProcess1"))
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                }
+                else if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                    // re-throw the original exception
+                    throw;
+                }
+                else
+                {
+                    // create a new exception object that wraps the original exception
+                    throw new ApplicationException("An error occurred - ", ex);
+                }
+            }
+        }
+
+    }
+}
+
+static void BusinessProcess1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+
+            checked
+            {
+                int calculatedValue = 4 / valueEntered;
+            }
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            throw invalidFormatException;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            throw unexpectedDivideByZeroException;
+
+        }
+    }
+}
